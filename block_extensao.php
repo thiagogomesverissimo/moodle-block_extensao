3492e5d16361eeba38a682ae1690c29ba359cdce
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-01-07 23:08:08 -0300

    sybase ok

--- block_extensao.php
+++ block_extensao.php
@@ -12,14 +12,21 @@ class block_extensao extends block_base {
     }
 
     public function get_content() {
-     
+        global $USER;
+        
+        #echo "<pre>"; var_dump(Sybase::oferecimentos('450312'));die();
+        #echo "<pre>"; var_dump();die();
+
         $this->content =  new stdClass;
+
+        $codpes = $USER->profile['codpes'];
+        if(!isset($codpes) || empty($codpes)) return $this->content;
 
         # Vamos listar todos cursos em que a pessoa em questão é ministrante
         $this->content->text = '';
-        foreach(Sybase::oferecimentos('123') as $oferecimento){
+        foreach(Sybase::oferecimentos($codpes) as $oferecimento){
             # Constrói a url da requisição GET para o formulário
-            $course = Helper::get_course($oferecimento);
+            $course = Helper::get_course($oferecimento['codofeatvceu']);
             if($course){
                 $url = new moodle_url('/course/view.php', [
                     'id' => $course->id
@@ -27,18 +34,18 @@ class block_extensao extends block_base {
                 $attr = [
                     'class'=>'btn btn-xs btn-primary'
                 ];
-                $this->content->text .= Sybase::curso($oferecimento)['nomcurceu'] . '<br>';
+                $this->content->text .= Sybase::curso($oferecimento['codofeatvceu'])['nomcurceu'] . '<br>';
                 $this->content->text .= html_writer::link($url, 'Ir Para Curso', $attr) . '<br><br>';
             } else {
                 $url = new moodle_url('/blocks/extensao/pages/novo_ambiente.php', [
-                    'codpes' => '123456',
-                    'oferecimento' => $oferecimento
+                    'codpes' => $codpes,
+                    'oferecimento' => $oferecimento['codofeatvceu']
                 ]);
         
                 $attr = [
                     'class'=>'btn btn-xs btn-success'
                 ];
-                $this->content->text .= Sybase::curso($oferecimento)['nomcurceu'] . '<br>';
+                $this->content->text .= Sybase::curso($oferecimento['codofeatvceu'])['nomcurceu'] . '<br>';
                 $this->content->text .= html_writer::link($url, 'Criar Ambiente', $attr) . '<br><br>';
             }
         }
